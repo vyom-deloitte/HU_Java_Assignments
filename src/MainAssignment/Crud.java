032857bd375260aa4a5b8573017cd03af92c93b6
Vyom Pant <vypant@deloitte.com>
2022-03-22 13:01:59 +0530

    new commit, added user registration and login functionality

--- src/MainAssignment/Crud.java
+++ src/MainAssignment/Crud.java
@@ -1,23 +1,22 @@
 package MainAssignment;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Crud implements Serializable
-{
-    static void display(ArrayList<Employee> al)
-    {
+import static MainAssignment.RegisterUser.*;
+
+public class Crud implements Serializable {
+    static void display(ArrayList<Employee> al) {
         System.out.println("\n            --------------Employee List---------------\n");
-        System.out.println(String.format("%-10s%-15s%-10s%-20s%-10s", "ID","Name","salary","contact-no","Email-Id"));
-        for(Employee e : al)
-        {
-            System.out.println(String.format("%-5s%-20s%-10s%-15s%-10s",e.id,e.name,e.salary,e.contact_no,e.email_id));
+        System.out.println(String.format("%-10s%-15s%-10s%-20s%-10s", "ID", "Name", "salary", "contact-no", "Email-Id"));
+        for (Employee e : al) {
+            System.out.println(String.format("%-5s%-20s%-10s%-15s%-10s", e.id, e.name, e.salary, e.contact_no, e.email_id));
         }
     }
 
     @SuppressWarnings("unchecked")
-    public static void main(String[] args)throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         int id;
         String name;
         float salary;
@@ -25,212 +24,233 @@ public class Crud implements Serializable
         String email_id;
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("1. Login");
+        System.out.println("2. Registration");
+        System.out.println("Enter your choice");
+        int c = sc.nextInt();
 
-        ArrayList<Employee> al = new ArrayList<Employee>();//Employee collection
-
-        File f = null;
-        FileInputStream fis = null;
-        ObjectInputStream ois = null;
-        FileOutputStream fos = null;
-        ObjectOutputStream oos =null;
-        try{
-
-            f = new File("C:\\Users\\vypant\\IdeaProjects\\HU_Java_Assignments\\src\\MainAssignment\\EmpDataBase.txt");
-            if(f.exists())// checking if file exists
-            {
-                fis = new FileInputStream(f);
-                ois = new ObjectInputStream(fis);
-                al = (ArrayList<Employee>)ois.readObject();
-                ois.close();
+        if (c == 1) {
+            try {
+                login();
+                System.out.println("Login Successful");
+            } catch (Exception e) {
+                System.out.println(e);
             }
 
-        }
-        catch(Exception exp){
-            System.out.println(exp);
-        }
 
-        do
-        {
-            System.out.println("\n********* Welcome to the Employee Management System **********\n");
-            System.out.println("1). Add Employee to the DataBase\n" +
-                    "2). Search for Employee\n" +
-                    "3). Edit Employee details\n" +
-                    "4). Delete Employee Details\n" +
-                    "5). Display all Employees working in this company\n" +
-                    "6). EXIT\n");
-            System.out.println("Enter your choice : ");
-            int ch = sc.nextInt();
+            ArrayList<Employee> al = new ArrayList<Employee>();//Employee collection
 
-            switch(ch)
-            {
-                case 1:System.out.println("\nEnter the following details to ADD list:\n");
-                    System.out.println("Enter ID :");
-                    id = sc.nextInt();
-                    System.out.println("Enter Name :");
-                    name = sc.next();
-                    System.out.println("Enter Salary :");
-                    salary = sc.nextFloat();
-                    System.out.println("Enter Contact No :");
-                    contact_no = sc.nextLong();
-                    System.out.println("Enter Email-ID :");
-                    email_id = sc.next();
-                    al.add(new Employee(id, name, salary, contact_no, email_id));
-                    oos= new ObjectOutputStream(new FileOutputStream(f));//writing employee data to our EmpDataBase
-                    oos.writeObject(al);
-                    oos.close();
-                    display(al);
-                    break;
+            File f = null;
+            FileInputStream fis = null;
+            ObjectInputStream ois = null;
+            FileOutputStream fos = null;
+            ObjectOutputStream oos = null;
+            try {
 
-                case 2: System.out.println("Enter the Employee ID to search :");
-                    id = sc.nextInt();
-                    int i=0;
-                    for(Employee e: al)
-                    {
-                        if(id == e.id)
-                        {
-                            System.out.println(e+"\n");
-                            i++;
-                        }
-                    }
-                    if(i == 0)
-                    {
-                        System.out.println("\nEmployee Details are not available, Please enter a valid ID!!");
-                    }
-                    break;
+                f = new File("C:\\Users\\vypant\\IdeaProjects\\HU_Java_Assignments\\src\\MainAssignment\\EmpDataBase.txt");
+                if (f.exists())// checking if file exists
+                {
+                    fis = new FileInputStream(f);
+                    ois = new ObjectInputStream(fis);
+                    al = (ArrayList<Employee>) ois.readObject();
+                    ois.close();
+                }
 
-                case 3: System.out.println("\nEnter the Employee ID to EDIT the details");
-                    id = sc.nextInt();
-                    int j=0;
-                    for(Employee e: al)
-                    {
-                        if(id == e.id)
-                        {
-                            j++;
-                            do{
-                                int ch1 =0;
-                                System.out.println("\nEDIT Employee Details :\n" +
-                                        "1). Employee ID\n" +
-                                        "2). Name\n" +
-                                        "3). Salary\n" +
-                                        "4). Contact No.\n" +
-                                        "5). Email-ID\n" +
-                                        "6). GO BACK\n");
-                                System.out.println("Enter your choice : ");
-                                ch1 = sc.nextInt();
-                                switch(ch1)
-                                {
-                                    case 1: System.out.println("\nEnter new Employee ID:");
-                                        e.id =sc.nextInt();
-                                        System.out.println(e+"\n");
-                                        break;
+            } catch (Exception exp) {
+                System.out.println(exp);
+            }
 
-                                    case 2: System.out.println("Enter new Employee Name:");
-                                        sc.nextLine();
-                                        e.name =sc.nextLine();
-                                        System.out.println(e+"\n");
-                                        break;
+            do {
+                System.out.println("\n********* Welcome to the Employee Management System **********\n");
+                System.out.println("1). Add User Data\n" +
+                        "2). Search for Employee\n" +
+                        "3). Update User Data\n" +
+                        "4). Delete User Data\n" +
+                        "5). Display/List User Data\n" +
+                        "6). EXIT\n");
+                System.out.println("Enter your choice : ");
+                int ch = sc.nextInt();
 
-                                    case 3: System.out.println("Enter new Employee Salary:");
-                                        e.salary =sc.nextFloat();
-                                        System.out.println(e+"\n");
-                                        break;
+                switch (ch) {
+                    case 1:
+                        System.out.println("\nEnter the following details to ADD list:\n");
+                        System.out.println("Enter ID :");
+                        id = sc.nextInt();
+                        System.out.println("Enter Name :");
+                        name = sc.next();
+                        System.out.println("Enter Salary :");
+                        salary = sc.nextFloat();
+                        System.out.println("Enter Contact No :");
+                        contact_no = sc.nextLong();
+                        System.out.println("Enter Email-ID :");
+                        email_id = sc.next();
+                        al.add(new Employee(id, name, salary, contact_no, email_id));
+                        oos = new ObjectOutputStream(new FileOutputStream(f));//writing employee data to our EmpDataBase
+                        oos.writeObject(al);
+                        oos.close();
+                        display(al);
+                        break;
 
-                                    case 4: System.out.println("Enter new Employee Contact No. :");
-                                        e.contact_no =sc.nextLong();
-                                        System.out.println(e+"\n");
-                                        break;
-
-                                    case 5: System.out.println("Enter new Employee Email-ID :");
-                                        e.email_id =sc.next();
-                                        System.out.println(e+"\n");
-                                        break;
-
-                                    case 6: j++;
-                                        break;
-
-                                    default : System.out.println("\nEnter a correct choice from the List :");
-                                        break;
-
-                                }
-                            }
-                            while(j==1);
-                        }
-                    }
-                    if(j == 0)
-                    {
-                        System.out.println("\nEmployee Details are not available, Please enter a valid ID!!");
-                    }
-
-                    break;
-
-                case 4: System.out.println("\nEnter Employee ID to DELETE from the Databse :");
-                    id = sc.nextInt();
-                    int k=0;
-                    try{
-                        for(Employee e: al)
-                        {
-                            if(id == e.id)
-                            {
-                                al.remove(e);
-                                display(al);
-                                k++;
+                    case 2:
+                        System.out.println("Enter the Employee ID to search :");
+                        id = sc.nextInt();
+                        int i = 0;
+                        for (Employee e : al) {
+                            if (id == e.id) {
+                                System.out.println(e + "\n");
+                                i++;
                             }
                         }
-                        if(k == 0)
-                        {
+                        if (i == 0) {
                             System.out.println("\nEmployee Details are not available, Please enter a valid ID!!");
                         }
-                    }
-                    catch(Exception ex){
-                        System.out.println(ex);
-                    }
-                    break;
+                        break;
 
-                case 5: try {
-                    al = (ArrayList<Employee>)ois.readObject();
+                    case 3:
+                        System.out.println("\nEnter the Employee ID to EDIT the details");
+                        id = sc.nextInt();
+                        int j = 0;
+                        for (Employee e : al) {
+                            if (id == e.id) {
+                                j++;
+                                do {
+                                    int ch1 = 0;
+                                    System.out.println("\nEDIT Employee Details :\n" +
+                                            "1). Employee ID\n" +
+                                            "2). Name\n" +
+                                            "3). Salary\n" +
+                                            "4). Contact No.\n" +
+                                            "5). Email-ID\n" +
+                                            "6). GO BACK\n");
+                                    System.out.println("Enter your choice : ");
+                                    ch1 = sc.nextInt();
+                                    switch (ch1) {
+                                        case 1:
+                                            System.out.println("\nEnter new Employee ID:");
+                                            e.id = sc.nextInt();
+                                            System.out.println(e + "\n");
+                                            break;
 
-                } catch (ClassNotFoundException e2) {
+                                        case 2:
+                                            System.out.println("Enter new Employee Name:");
+                                            sc.nextLine();
+                                            e.name = sc.nextLine();
+                                            System.out.println(e + "\n");
+                                            break;
 
-                    System.out.println(e2);
-                } catch (Exception e2) {
+                                        case 3:
+                                            System.out.println("Enter new Employee Salary:");
+                                            e.salary = sc.nextFloat();
+                                            System.out.println(e + "\n");
+                                            break;
 
-                    System.out.println(e2);
+                                        case 4:
+                                            System.out.println("Enter new Employee Contact No. :");
+                                            e.contact_no = sc.nextLong();
+                                            System.out.println(e + "\n");
+                                            break;
+
+                                        case 5:
+                                            System.out.println("Enter new Employee Email-ID :");
+                                            e.email_id = sc.next();
+                                            System.out.println(e + "\n");
+                                            break;
+
+                                        case 6:
+                                            j++;
+                                            break;
+
+                                        default:
+                                            System.out.println("\nEnter a correct choice from the List :");
+                                            break;
+
+                                    }
+                                }
+                                while (j == 1);
+                            }
+                        }
+                        if (j == 0) {
+                            System.out.println("\nEmployee Details are not available, Please enter a valid ID!!");
+                        }
+
+                        break;
+
+                    case 4:
+                        System.out.println("\nEnter Employee ID to DELETE from the Databse :");
+                        id = sc.nextInt();
+                        int k = 0;
+                        try {
+                            for (Employee e : al) {
+                                if (id == e.id) {
+                                    al.remove(e);
+                                    display(al);
+                                    k++;
+                                }
+                            }
+                            if (k == 0) {
+                                System.out.println("\nEmployee Details are not available, Please enter a valid ID!!");
+                            }
+                        } catch (Exception ex) {
+                            System.out.println(ex);
+                        }
+                        break;
+
+                    case 5:
+                        try {
+                            al = (ArrayList<Employee>) ois.readObject();
+
+                        } catch (ClassNotFoundException e2) {
+
+                            System.out.println(e2);
+                        } catch (Exception e2) {
+
+                            System.out.println(e2);
+                        }
+                        display(al);
+                        break;
+
+                    case 6:
+                        try {
+                            fos = new FileOutputStream(f);
+                            oos = new ObjectOutputStream(fos);
+                            oos.writeObject(al);
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        } catch (Exception e2) {
+                            e2.printStackTrace();
+                        } finally {
+                            try {
+                                fis.close();
+                                ois.close();
+                                fos.close();
+                                oos.close();
+                            } catch (Exception e1) {
+                                e1.printStackTrace();
+                            }
+
+                        }
+                        System.out.println("\nYou have chosen EXIT !! Saving Files and closing the tool.");
+                        sc.close();
+                        System.exit(0);
+                        break;
+
+                    default:
+                        System.out.println("\nEnter a correct choice from the List :");
+                        break;
+
                 }
-                    display(al);
-                    break;
-
-                case 6: try {
-                    fos = new FileOutputStream(f);
-                    oos = new ObjectOutputStream(fos);
-                    oos.writeObject(al);
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                catch(Exception e2){
-                    e2.printStackTrace();
-                }
-                finally{
-                    try {
-                        fis.close();
-                        ois.close();
-                        fos.close();
-                        oos.close();
-                    } catch (Exception e1) {
-                        e1.printStackTrace();
-                    }
-
-                }
-                    System.out.println("\nYou have chosen EXIT !! Saving Files and closing the tool.");
-                    sc.close();
-                    System.exit(0);
-                    break;
-
-                default : System.out.println("\nEnter a correct choice from the List :");
-                    break;
-
             }
-        }
-        while(true);
-    }
+            while (true);
 
+        }//if login successful closed
+        else if (c == 2) {//if new user wants to register
+            registration();
+            WriteRegData();
+            System.out.println("Registration successful.");
+
+        } else {
+            System.out.println("wrong choice");
+        }
+
+    }
 }
---
+++ src/MainAssignment/RegisterUser.java
@@ -0,0 +1,60 @@
+package MainAssignment;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class RegisterUser {
+    //    Scanner sc = new Scanner(System.in);
+    static HashMap<String, String> loginDetails = new HashMap<>();
+
+    public static void WriteRegData() {//writing user registration login data
+        PrintWriter pw = null;
+        String fileName = "C:\\Users\\vypant\\IdeaProjects\\HU_Java_Assignments\\src\\MainAssignment\\LoginData.csv";
+        try {
+            pw = new PrintWriter(fileName);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (String key : loginDetails.keySet()) {
+            pw.println(key + "," + loginDetails.get(key));
+        }
+        pw.flush();
+        pw.close();
+    }
+
+    static void login() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---Welcome to Login Page---");
+        System.out.println("Enter Username : ");
+        String userName = sc.nextLine();
+        System.out.println("Enter password : ");
+        String password = sc.nextLine();
+
+        //if (!loginDetails.containsKey(userName)) throw new InputMismatchException("Invalid details");
+        //if (password != loginDetails.get(userName)) throw new InputMismatchException("Invalid details");
+    }
+
+    static void registration() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Username : ");
+        String userName = sc.nextLine();
+        System.out.println("Enter Password : ");
+        String password = sc.nextLine();
+        System.out.println("Re-Enter Password : ");
+        String password2 = sc.nextLine();
+        if (!password.equals(password2)) throw new InputMismatchException("Passwords don't match");
+        if (!loginDetails.isEmpty() && loginDetails.containsKey(userName))
+            throw new InputMismatchException("Already Registered.");
+        loginDetails.put(userName, password);
+
+    }
+}
+
+
+
+
+
